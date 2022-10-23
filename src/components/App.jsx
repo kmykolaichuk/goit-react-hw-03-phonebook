2ff863c7398c7f42391de8c531548210641763db
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-10-23 14:21:53 +0300

    Add contacts to LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { Filter } from './Filter/Filter';
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -45,6 +40,21 @@ class App extends React.Component {
       contacts: prevState.contacts.filter(contact => idContact !== contact.id),
     }));
   };
+
+  componentDidMount() {
+    const localStorageContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(localStorageContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   render() {
     const filteredContacts = this.filterContact();
